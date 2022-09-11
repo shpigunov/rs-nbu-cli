5d3c992e3f01f910244b0da231b262e67634c809
Anton Shpigunov <shpigunov@gmail.com>
2022-09-11 23:23:31 +0300

    Enum serialization with Strum

--- src/currency.rs
+++ src/currency.rs
@@ -1,26 +1,10 @@
-#[derive(PartialEq, Eq, Debug)]
+use strum::Display;
+use strum_macros::EnumString;
+
+#[derive(PartialEq, Eq, Debug, EnumString, Display)]
 pub enum Currency {
+    #[strum(serialize = "EUR")]
     Eur,
+    #[strum(serialize = "USD")]
     Usd,
-}
-
-impl std::str::FromStr for Currency {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "EUR" => Ok(Currency::Eur),
-            "USD" => Ok(Currency::Usd),
-            _ => Err(format!("Unknown currency: {}", s)),
-        }
-    }
-}
-
-impl std::fmt::Display for Currency {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Currency::Eur => write!(f, "EUR"),
-            Currency::Usd => write!(f, "USD"),
-        }
-    }
 }
